2f3a42941d37ea161bdc1b5f31854efb3955f5c2
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2024-04-12 13:14:40 -0400

    add Do it yourself Exercise

---
+++ ForNextIf/ForNextIf/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub btnCount_Click(sender As Object, e As EventArgs) Handles btnCount.Click
+
+        Dim iMax As Integer
+        Dim stOddOrEven As String
+        Dim x As Integer
+
+        iMax = InputBox("what number do you want to count up to?")
+        stOddOrEven = InputBox("do you want odd numbers or even numbers?")
+
+        If stOddOrEven = "even" Then
+            For x = 2 To iMax Step 2
+                MsgBox(x)
+            Next
+        ElseIf stOddOrEven = "odd" Then
+            For x = 1 To iMax Step 2
+                MsgBox(x)
+            Next
+
+        End If
+    End Sub
+End Class
---
+++ ForNextIf/ForNextIf/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ForNextIf")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ForNextIf")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ab520046-720b-4512-b5e6-18ab5767835e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
