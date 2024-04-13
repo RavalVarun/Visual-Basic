13078da72cdb0573cb92e799ee726d6e2a62c95a
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2024-04-13 15:04:16 -0400

    add Array Exercise

---
+++ VariableArrays/WindowsApp1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim astFruits(4) As String
+
+        astFruits(0) = "Banana"
+        astFruits(1) = "Apple"
+        astFruits(2) = "Mango"
+        astFruits(3) = "Strawberry"
+        astFruits(4) = "Grapes"
+
+        Dim i As Integer
+
+        For i = 0 To 4
+            MsgBox(astFruits(i))
+        Next
+
+    End Sub
+End Class
