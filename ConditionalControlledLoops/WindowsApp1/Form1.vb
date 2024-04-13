6f4ce542702eb931caa54beb5baf6b3f7b6eb5ca
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2024-04-13 14:52:18 -0400

    add ConditionalControlledLoop exercise

---
+++ ConditionalControlledLoops/WindowsApp1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim stAge As String
+        Dim iAge As Integer
+
+        Do While IsNumeric(stAge) = False
+            stAge = InputBox("please enter your age in years")
+        Loop
+
+        iAge = CInt(stAge)
+
+        MsgBox("You are " & iAge & " years old")
+
+    End Sub
+End Class
