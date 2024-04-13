b2a1048fc702dfdcfc6054f447c66d88efdad3ca
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2024-04-13 15:28:03 -0400

    create and add layout for all exercises

---
+++ ArrayExercise/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExercise1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExercise2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnExercise3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnExercise4 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnExercise5 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnExercise6 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnExercise7 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnExercise8 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnExercise1
+        '
+        Me.btnExercise1.Location = New System.Drawing.Point(84, 34)
+        Me.btnExercise1.Name = "btnExercise1"
+        Me.btnExercise1.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise1.TabIndex = 0
+        Me.btnExercise1.Text = "Exercise 1"
+        Me.btnExercise1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(217, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(351, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Output Each Item in a separate message one after another"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(217, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(323, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Output all items in the same msg box in separate lines"
+        '
+        'btnExercise2
+        '
+        Me.btnExercise2.Location = New System.Drawing.Point(84, 83)
+        Me.btnExercise2.Name = "btnExercise2"
+        Me.btnExercise2.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise2.TabIndex = 2
+        Me.btnExercise2.Text = "Exercise 2"
+        Me.btnExercise2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(217, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(235, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Add up all items and display the output"
+        '
+        'btnExercise3
+        '
+        Me.btnExercise3.Location = New System.Drawing.Point(84, 132)
+        Me.btnExercise3.Name = "btnExercise3"
+        Me.btnExercise3.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise3.TabIndex = 4
+        Me.btnExercise3.Text = "Exercise 3"
+        Me.btnExercise3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(217, 194)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(180, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Calculate the avg of the items"
+        '
+        'btnExercise4
+        '
+        Me.btnExercise4.Location = New System.Drawing.Point(84, 181)
+        Me.btnExercise4.Name = "btnExercise4"
+        Me.btnExercise4.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise4.TabIndex = 6
+        Me.btnExercise4.Text = "Exercise 4"
+        Me.btnExercise4.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(217, 243)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(339, 16)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Add items which are greater than 20 and display the total"
+        '
+        'btnExercise5
+        '
+        Me.btnExercise5.Location = New System.Drawing.Point(84, 230)
+        Me.btnExercise5.Name = "btnExercise5"
+        Me.btnExercise5.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise5.TabIndex = 8
+        Me.btnExercise5.Text = "Exercise 5"
+        Me.btnExercise5.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(217, 292)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(126, 16)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Find the largest item"
+        '
+        'btnExercise6
+        '
+        Me.btnExercise6.Location = New System.Drawing.Point(84, 279)
+        Me.btnExercise6.Name = "btnExercise6"
+        Me.btnExercise6.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise6.TabIndex = 10
+        Me.btnExercise6.Text = "Exercise 6"
+        Me.btnExercise6.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(217, 341)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(135, 16)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Find the smallest item"
+        '
+        'btnExercise7
+        '
+        Me.btnExercise7.Location = New System.Drawing.Point(84, 328)
+        Me.btnExercise7.Name = "btnExercise7"
+        Me.btnExercise7.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise7.TabIndex = 12
+        Me.btnExercise7.Text = "Exercise 7"
+        Me.btnExercise7.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(217, 390)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(539, 16)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Replace each item in an array with a new value that twice as big then output the " &
+    "new values"
+        '
+        'btnExercise8
+        '
+        Me.btnExercise8.Location = New System.Drawing.Point(84, 377)
+        Me.btnExercise8.Name = "btnExercise8"
+        Me.btnExercise8.Size = New System.Drawing.Size(103, 43)
+        Me.btnExercise8.TabIndex = 14
+        Me.btnExercise8.Text = "Exercise 8"
+        Me.btnExercise8.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.btnExercise8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnExercise7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnExercise6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnExercise5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnExercise4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnExercise3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExercise2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExercise1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExercise1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnExercise2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnExercise3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnExercise4 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnExercise5 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnExercise6 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnExercise7 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnExercise8 As Button
+End Class
---
+++ ArrayExercise/WindowsApp1/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnExercise1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnExercise2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExercise3.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnExercise4.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnExercise5.Click
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles btnExercise6.Click
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles btnExercise7.Click
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles btnExercise8.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
