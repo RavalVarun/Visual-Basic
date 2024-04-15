64c4bc0104b499680b02c31a59e0571dfb202e2a
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2024-04-15 11:08:32 -0400

    add rest of the solutions AND mark each solutions with comments to distinguish

--- ArrayExercise/WindowsApp1/Form1.vb
+++ ArrayExercise/WindowsApp1/Form1.vb
@@ -66,26 +66,149 @@
 
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExercise3.Click
+        'Add up all items and display the output
 
+        Dim aiData(4) As Integer
+
+        aiData(0) = 9
+        aiData(1) = 14
+        aiData(2) = 17
+        aiData(3) = 15
+        aiData(4) = 85
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0to4
+                iTotal = iTotal + aiData(i)
+        Next
+
+        MsgBox("Total is " & iTotal)
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnExercise4.Click
+        'Calculate the avg of the items
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 9
+        aiData(1) = 14
+        aiData(2) = 17
+        aiData(3) = 15
+        aiData(4) = 85
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0 To 4
+            iTotal = aiData(i)
+        Next
+
+        MsgBox("The average number is " & iTotal / 5)
+
+
 
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnExercise5.Click
+        'Add items which are greater than 20 and display the total
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 9
+        aiData(1) = 14
+        aiData(2) = 17
+        aiData(3) = 15
+        aiData(4) = 85
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0to4
+                If aiData(i) > 20 Then
+                iTotal = iTotal + aiData(i)
+            End If
+        Next
+
+        MsgBox("Items bigger than 20's total is " & iTotal)
 
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles btnExercise6.Click
+        'Find the largest item
 
+        Dim aiData(4) As Integer
+
+        aiData(0) = 9
+        aiData(1) = 14
+        aiData(2) = 17
+        aiData(3) = 15
+        aiData(4) = 85
+
+        Dim i As Integer
+        Dim iMax As Integer
+
+
+        For i = 0 To 4
+            If aiData(i) > iMax Then
+                iMax = aiData(i)
+            End If
+        Next
+
+        MsgBox("The largest number is " & iMax)
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles btnExercise7.Click
+        'Find the smallest item
 
+        Dim aiData(4) As Integer
+
+        aiData(0) = 9
+        aiData(1) = 14
+        aiData(2) = 17
+        aiData(3) = 15
+        aiData(4) = 85
+
+        Dim i As Integer
+        Dim iMin As Integer
+
+        iMin = 25000
+
+        For i = 0 To 4
+            If aiData(i) < iMin Then
+                iMin = aiData(i)
+            End If
+        Next
+
+        MsgBox("The smallest number is " & iMin)
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles btnExercise8.Click
+        'Replace each item in an array with a new value that twice as big then output the new values
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 9
+        aiData(1) = 14
+        aiData(2) = 17
+        aiData(3) = 15
+        aiData(4) = 85
+
+        Dim i As Integer
+
+        For i = 0 To 4
+            aiData(i) = aiData(i) * 2
+        Next
+
+        Dim stOut As String
+
+        For i = 0 To 4
+            stOut = stOut & aiData(i) & vbNewLine
+
+        Next
+
+        MsgBox(stOut)
+
 
     End Sub
 
